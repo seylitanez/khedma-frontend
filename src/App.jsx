4341715d7cb066311b157813226d9f6e833fe120
aitanez lyes <lyes.zenati@se.univ-bejaia.dz>
2023-02-24 20:41:23 +0100

    initialization du projet khedma (frontend)

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from '../public/assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>hello world</h1>
+    </div>
+  )
+}
+
+export default App
