a09dc2e22821fc268d920413a8f5afbfc6de5012
Yaici-Yacine <yacine.yaici@se.univ-bejaia.dz>
2023-02-27 12:18:29 +0100

    path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    port:9710,
+  },
+  resolve: {
+    alias: {
+      '@':path.resolve(__dirname,'src'),
+      '@components':path.resolve(__dirname,'src/components'),
+      '@pages':path.resolve(__dirname,'src/pages'),
+      '@utils':path.resolve(__dirname,'src/utils'),
+      '@utils':path.resolve(__dirname,'src/utils'),
+    }
+  }
 })
