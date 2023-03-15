71ae8e0197bc5defcd39a59cfc5d58925795accb
Yaici-Yacine <yacine.yaici@se.univ-bejaia.dz>
2023-03-15 22:27:25 +0100

    footer

--- src/components/public/footer/Footer.jsx
+++ src/components/public/footer/Footer.jsx
@@ -1,13 +1,14 @@
 import "./footer.scss" ;
-import {} from "react-icons/fa";
+import { BsTelephoneFill, BsFacebook, BsLinkedin } from "react-icons/bs";
+import { HiMail } from "react-icons/hi";
+import { AiFillTwitterCircle } from "react-icons/ai";
 import {Logo} from '@image'
 import {Input,Buttun} from '@p-components';
-import { Link,useNavigate } from "react-router-dom";
+import { Link} from "react-router-dom";
 import {LangueContext} from "@context/langue";
 import { useContext ,useState } from 'react';
 const Footer = () => {
     const {lang} = useContext(LangueContext);
-    const nav=useNavigate()
     const {lien,accueil,trouver_emploi,blog,a_propos}=lang.footer.menu;
     const {contact,tel,mail}=lang.footer.contact;
     const {slogon,copyRight,terms,cockie,privatep,license}=lang.footer.police;
@@ -15,18 +16,19 @@ const Footer = () => {
         <div className="footer">
             <div className="footer_info">
                 <div className="app_name">
-                    .logo
-                    <ul>
-                        <li><img src={Logo}/></li>
-                        <li><h4 >{slogon}</h4></li>
-                        <li>
-                            <Input type="email" id="emails" placeholder="email@exemple.com"/>
-                            <Buttun name="subscribeFooter" id="subscribe">{lang.footer.btn}</Buttun>
-                        </li>
-                    </ul>
+                    <div className='logo'>
+                        <img src={Logo}/>
+                    </div>
+                    <div className="slogo">
+                        <p>{slogon}</p>
+                    </div>
+                    <div className="sub">
+                        <Input type="email" id="emails" placeholder="email@exemple.com"/>
+                        <Buttun id="subscribe">{lang.footer.btn}</Buttun>
+                    </div>
                 </div>
                 <div className="link">
-                    <h4>{lien}</h4>
+                    <h3>{lien}</h3>
                     <ul>
                         <li><Link to='./home'>{accueil}</Link></li>
                         <li><Link to='./jobSearch'>{trouver_emploi}</Link></li>
@@ -34,20 +36,24 @@ const Footer = () => {
                         <li><Link to='./a_propos'>{a_propos}</Link></li>
                     </ul>
                 </div>
-                <div className="col3">
-                <h4>{contact}</h4>
-                <ul>
-                    <li>{tel}</li>
-                    <li>{mail}</li>
-                    <li><div className="social-links"></div></li>
+                <div className="contact">
+                    <h3>{contact}</h3>
+                    <ul>
+                        <li><BsTelephoneFill size={15}/> {tel}</li>
+                        <li><HiMail size={20}/> {mail}</li>
+                        <li><div className="social-links">
+                            <AiFillTwitterCircle className="tw"size={25}/>
+                            <BsLinkedin className="li"size={25}/>
+                            <BsFacebook className="fb"size={25}/>
+                        </div></li>
                    </ul>
                 </div>
             </div>
             <div className="policy">
-                <div className="col1">
+                <div className="copy">
                     <p>{copyRight}</p>
                 </div>
-                <div className="col2">
+                <div className="policy_link">
                     <Link to="#">{terms}</Link>
                     <Link to="#">{cockie}</Link>
                     <Link to="#">{privatep}</Link>
