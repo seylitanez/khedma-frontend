63c661763a0e135ab6f86cae33c6d30d0fe817e1
Yaici-Yacine <yacine.yaici@se.univ-bejaia.dz>
2023-03-15 11:05:18 +0100

    ttt

---
+++ src/components/public/sideBar/SideBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Login} from '@p-components'
+import "./sideBar.scss";
+
+export default function SideBar() {
+    return (
+        <section className='sideBar'>
+            <Login/>
+        </section>
+    )
+}
--- src/pages/public/jobSearch/JobSearch.jsx
+++ src/pages/public/jobSearch/JobSearch.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import {Annonce} from '@p-components'
+import {Annonce,SideBar} from '@p-components'
 import './jobSearch.scss'
 export default function JobSearch() {
   return (
-    <div className='jobSearch'>
-      
+  <div className='jobSearch'>
       <div className="search">
 
       </div>
+      <SideBar/>
       <div className="list">
         <Annonce/>
       </div>
