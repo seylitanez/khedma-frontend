1b9a6be35cab504d5613190af6c968485be2b426
Yaici-Yacine <yacine.yaici@se.univ-bejaia.dz>
2023-04-13 01:08:45 +0100

    form

--- src/components/public/form_ins/FormIns.jsx
+++ src/components/public/form_ins/FormIns.jsx
@@ -10,11 +10,11 @@ export default function FormIns() {
     const {h2,nom,prenom,username,email,password,rpassword,gender,next}=lang.auth.signin;
     const {male,femme}=gender;
     const [user,setUser]=useState({nomUtilisateur:"",motDePasse:"",nom:"",prenom:"",adresseMail:"",genre:""})
+    const [psw,setpsw]=useState("")
     const onchange=(e)=>setUser({...user,[e.target.name]:e.target.value})
-    const checkPassword=e=>{   
-        if (e.target.value!=user.motDePasse) {
-            
-        }
+    const checkPassword=e=>{
+        if (e.target.id==="rmdp" ) setpsw(e.target.value)
+        if (psw===user.motDePasse) onchange(e)        
     }
     const onsubmit=(e)=>{
         e.preventDefault();
@@ -26,26 +26,26 @@ export default function FormIns() {
         <form onSubmit={onsubmit}>
             <h2>{h2}</h2>
             <div className={"ins__group "}>
-                <Input type="text" id="nom" value={user.nom} onChange={onchange}>{nom}</Input>
+                <Input type="text" id="nom" name="nom" value={user.nom} onChange={onchange}>{nom}</Input>
             </div>
             <div className={"ins__group "}>
-                <Input type="text" id="prenom" value={user.prenom} onChange={onchange}>{prenom}</Input>
+                <Input type="text" id="prenom" name="prenom" value={user.prenom} onChange={onchange}>{prenom}</Input>
             </div>
             <div className={"ins__group "}>
-                <Input type="text" id="username" value={user.nomUtilisateur} onChange={onchange}>{username}</Input>
+                <Input type="text" id="username" name="nomUtilisateur" value={user.nomUtilisateur} onChange={onchange}>{username}</Input>
             </div>
             <div className={"ins__group "}>
-                <Input type="email" id="email" value={user.adresseMail} onChange={onchange}>{email}</Input>
+                <Input type="email" id="email" name="adresseMail" value={user.adresseMail} onChange={onchange}>{email}</Input>
             </div>
             <div className={"ins__group "}>
-                <Input type="password" id='mdp' value={user.motDePasse} onChange={onchange}>{password}</Input>
+                <Input type="password" id='mdp' name="motDePasse" value={user.motDePasse} onChange={checkPassword}>{password}</Input>
             </div>
             <div className={"ins__group "}>
-                <Input type="password" id='rmdp' value={{}}>{rpassword}</Input>
+                <Input type="password" id='rmdp' name="motDePasse" value={psw} onChange={checkPassword}>{rpassword}</Input>
             </div>
             <div className={"ins__group "}>
-                <Input type="checkbox" id='male' value={{}}>{male}</Input>
-                <Input type="checkbox" id='femme' value={{}}>{femme}</Input>
+                <Input type="radio" id='male'name="genre" value="male"onChange={onchange} >{male}</Input>
+                <Input type="radio" id='femme'name="genre" value='femme'onChange={onchange}>{femme}</Input>
             </div>
             <div className="ins__next">
                 <Buttun id="sing">{next}</Buttun>
