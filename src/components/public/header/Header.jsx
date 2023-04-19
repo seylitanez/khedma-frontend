d385ae2632da8201d00b34132ae201272d7f701e
Yaici-Yacine <yacine.yaici@se.univ-bejaia.dz>
2023-04-19 15:52:53 +0100

    link

--- src/components/public/header/Header.jsx
+++ src/components/public/header/Header.jsx
@@ -2,46 +2,37 @@ import React from 'react'
 import {dz,france,royaume_uni} from "@image"
 import './header.scss';
 import { Link ,useNavigate } from 'react-router-dom';
-import {Buttun , Logo} from '@p-components';
 import {LangueContext} from "@context/langue";
 import { useContext ,useState } from 'react';
-import FormIns from '../form_ins/FormIns';
-import Login from '../login/Login';
+import { Buttun, FormIns, Logo } from '@p-components/index';
 
 
 function useShowPopup(bool) {
-
     const [popUp,setPopUp] =useState();
-
     const popupShow=(bool)=>{
         console.log(bool);
         if (bool) {
              setPopUp(
-             <div className='popup' onClick={()=>setPopUp(false)}>
-                 <div className='popup__ins__top'>
-                     <h3>Suivez les étapes pour finaliser la création de votre CV</h3>
-                     <div className='etapes'>
-                     <div className='etape'>1</div>
-                     <div className='etape'>2</div>
-                     <div className='etape'>3</div>
-                     <div className='etape'>4</div>
-                     <div className='etape'>5</div>
-                 </div>
-             </div>
-             <div className='popup__container' onClick={()=>setPopUp(true)}>
-                 {/* <Login/> */}
-                 <FormIns/>
-             </div>
-            
-         </div>)
-    
+            <div className='popup' onClick={()=>setPopUp(false)}>
+                <div className='popup__ins__top'>
+                    <h3>Suivez les étapes pour finaliser la création de votre CV</h3>
+                    <div className='etapes'>
+                        <div className='etape'>1</div>
+                        <div className='etape'>2</div>
+                        <div className='etape'>3</div>
+                        <div className='etape'>4</div>
+                        <div className='etape'>5</div>
+                    </div>
+                </div>
+                <div className='popup__container' onClick={()=>setPopUp(true)}>
+                    {/* <Login/> */}
+                    <FormIns/>
+                </div>
+            </div>)
         }
         else
-         setPopUp(null)
-
+        setPopUp(null)
     }
-    
-
     return [popUp,popupShow]
 }
 
@@ -50,22 +41,19 @@ export default function Header() {
     const nav=useNavigate()
     const {accueil,trouver_emploi,blog,a_propos}=lang.header.menu;
     const {connexion,inscription}=lang.header.auth;
-
     const [popUp,popupShow]=useShowPopup(false)
-
-
     return (
         <header>
             <nav>
                 <Link to='./' className="logo">
-                    <Logo />
+                    <Logo/>
                 </Link>
                 <div className="menu">
                     <ul>
-                        <li><Link to='./home'>{accueil}</Link></li>
-                        <li><Link to='./jobSearch'>{trouver_emploi}</Link></li>
-                        <li><Link to='./blog'>{blog}</Link></li>
-                        <li><Link to='./a_propos'>{a_propos}</Link></li>
+                        <li><Link to='/home'>{accueil}</Link></li>
+                        <li><Link to='/jobSearch'>{trouver_emploi}</Link></li>
+                        <li><Link to='/blog'>{blog}</Link></li>
+                        <li><Link to='/a_propos'>{a_propos}</Link></li>
                     </ul>
                 </div>
                 <div className="log">
