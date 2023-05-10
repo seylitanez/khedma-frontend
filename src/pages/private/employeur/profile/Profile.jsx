f7a907cb4e57907516fd52a01f92ded3406744f9
Yaici-Yacine <yacine.yaici@se.univ-bejaia.dz>
2023-05-10 18:26:31 +0100

    v

--- src/pages/private/employeur/profile/Profile.jsx
+++ src/pages/private/employeur/profile/Profile.jsx
@@ -1,41 +1,48 @@
-import React from 'react'
-import "./profile.scss"
-import { useState, useEffect, useRef } from 'react'
+import React from "react";
+import "./profile.scss";
+import { useState, useEffect, useRef } from "react";
 import { userService } from "@service/index";
-import { useParams } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import { CgProfile } from 'react-icons/cg';
-import { Outlet } from 'react-router-dom';
-import { BsList, BsListUl } from 'react-icons/bs';
-import { GrAdd } from 'react-icons/gr';
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { CgProfile } from "react-icons/cg";
+import { Outlet } from "react-router-dom";
+import { BsList, BsListUl } from "react-icons/bs";
+import { GrAdd } from "react-icons/gr";
 
 export default function Profile() {
-    const param=useParams()
-    const [user, setUser] = useState({})
-     useEffect(() => {
-        userService.getUser()
-            .then(res => setUser(res.data))
-            .catch(err => console.log(err))
-    }, [])
+    const param = useParams();
+    const [user, setUser] = useState({});
+    useEffect(() => {
+        userService
+            .getUser()
+            .then((res) => setUser(res.data))
+            .catch((err) => console.log(err));
+    }, []);
     return (
         <div>
-            <div className='profile_employeur'>
+            <div className="profile_employeur">
                 <div className="sidebar">
                     <div className="sidebar__item">
-                        <CgProfile size={20} className='prf' />
-                        <Link to={param.nomUtilisateur} >Profile</Link>
+                        <CgProfile size={20} className="prf" />
+                        <Link to={param.nomUtilisateur}>Profile</Link>
                     </div>
                     <div className="sidebar__item">
-                        <BsListUl size={20} className='prf' />
-                        <Link to={param.nomUtilisateur + '/listAnnonce'}>listAnnonce</Link>
+                        <BsListUl size={20} className="prf" />
+                        <Link to={param.nomUtilisateur + "/listAnnonce"}>
+                            listAnnonce
+                        </Link>
                     </div>
                     <div className="sidebar__item">
-                        <GrAdd size={20} className='prf' />
-                        <Link to={param.nomUtilisateur + '/ajouterAnnonce'}>AjouterAnnonce</Link>
+                        <GrAdd size={20} className="prf" />
+                        <Link to={param.nomUtilisateur + "/ajouterAnnonce"}>
+                            AjouterAnnonce
+                        </Link>
                     </div>
                     <div className="sidebar__item">
-                        <BsList size={20} className='prf' />
-                        <Link to={param.nomUtilisateur + '/listPostulent'}>List Postulent</Link>
+                        <BsList size={20} className="prf" />
+                        <Link to={param.nomUtilisateur + "/listPostulent"}>
+                            List Postulent
+                        </Link>
                     </div>
                 </div>
                 <main className="main">
@@ -43,5 +50,5 @@ export default function Profile() {
                 </main>
             </div>
         </div>
-    )
+    );
 }
--- src/pages/public/home/Home.jsx
+++ src/pages/public/home/Home.jsx
@@ -1,58 +1,82 @@
-import React, { useLayoutEffect } from 'react'
+import React, { useLayoutEffect } from "react";
 import "./home.scss";
-import { useContext,useState,useEffect} from 'react';
-import { ImageHome, Search } from '@p-components/index';
-import { LangueContext } from '@context/langue';
-import { AnnonceContext } from '@context/Annonce';
-import ImagePassword from '@p-components/imageHome/ImagePassword';
-import ImageProfil from '@p-components/imageHome/ImageProfil';
+import { useContext, useState, useEffect } from "react";
+import { ImageHome, Search } from "@p-components/index";
+import { LangueContext } from "@context/langue";
+import { AnnonceContext } from "@context/Annonce";
+import ImagePassword from "@p-components/imageHome/ImagePassword";
+import ImageProfil from "@p-components/imageHome/ImageProfil";
 
-function useImage(indexInitial){
-  const [index,setIndex]=useState(indexInitial)
-  return[index,setIndex]
+function useImage(indexInitial) {
+    const [index, setIndex] = useState(indexInitial);
+    return [index, setIndex];
 }
 export default function Home() {
-  const {lang} = useContext(LangueContext);
-  const { setSearch } = useContext(AnnonceContext)
-  const {h1,h2}=lang.home;
-  const images=[{image:<ImageHome />,description:<h2 id='desc1'>trouvez le travail qui vous corespond en quelques mots seulement</h2>}
-  ,{image:<ImageProfil/>,description:<h2 id='desc2'>creez votre profile et et deposez votre cv pour etre dans les meilleures coups</h2>}
-  ,{image:<ImagePassword />,description:<h2 id='desc1'>inscrivez vous et consulter pour avoir les meilleurs offres d'emploi grace a khedma</h2>}]
-  const [index,setIndex]=useImage(0)
-  let txt=""
-  let i=-1
-  const [titre,setTitre]=useState("")
-   useLayoutEffect(()=>{
-     const timer=setInterval(()=>{
-       i++
-       if (i<=h1.length) {
-         setTitre(t=>t+=h1.charAt(i))
-       }else{
-         clearInterval(timer)
-       }
-       return ()=>{
-         clearInterval(timer)
-       }
-     },80)
-   },[])
-   useLayoutEffect(()=>{
-    const timer=setInterval(()=>{
-        setIndex(i=>i=(i+1)%3)
-        return ()=>{
-          clearInterval(timer)
-        }
-    },5000)
-   },[])
-  return (
-    <main className='home'>
-      <div className="img">
-      {images[index].image}
-      </div>
-      <div className="left">
-        <h1 className='notranslate'>{titre}</h1>
-        {images[index].description}
-        <Search setSearch={setSearch} parent='home'/>
-      </div>
-    </main>
-  )
+    const { lang } = useContext(LangueContext);
+    const { setSearch } = useContext(AnnonceContext);
+    const { h1, h2 } = lang.home;
+    const images = [
+        {
+            image: <ImageHome />,
+            description: (
+                <h2 id="desc1">
+                    trouvez le travail qui vous corespond en quelques mots
+                    seulement
+                </h2>
+            ),
+        },
+        {
+            image: <ImageProfil />,
+            description: (
+                <h2 id="desc2">
+                    creez votre profile et et deposez votre cv pour etre dans
+                    les meilleures coups
+                </h2>
+            ),
+        },
+        {
+            image: <ImagePassword />,
+            description: (
+                <h2 id="desc1">
+                    inscrivez vous et consulter pour avoir les meilleurs offres
+                    d'emploi grace a khedma
+                </h2>
+            ),
+        },
+    ];
+    const [index, setIndex] = useImage(0);
+    let txt = "";
+    let i = -1;
+    const [titre, setTitre] = useState("");
+    useLayoutEffect(() => {
+        const timer = setInterval(() => {
+            i++;
+            if (i <= h1.length) {
+                setTitre((t) => (t += h1.charAt(i)));
+            } else {
+                clearInterval(timer);
+            }
+            return () => {
+                clearInterval(timer);
+            };
+        }, 80);
+    }, []);
+    useLayoutEffect(() => {
+        const timer = setInterval(() => {
+            setIndex((i) => (i = (i + 1) % 3));
+            return () => {
+                clearInterval(timer);
+            };
+        }, 5000);
+    }, []);
+    return (
+        <main className="home">
+            <div className="img">{images[index].image}</div>
+            <div className="left">
+                <h1 className="notranslate">{titre}</h1>
+                {images[index].description}
+                <Search setSearch={setSearch} parent="home" />
+            </div>
+        </main>
+    );
 }
