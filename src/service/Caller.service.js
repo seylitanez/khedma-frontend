dde5152a436b06e31b359d903e4f18ff8a83b62b
Yaici-Yacine <yacine.yaici@se.univ-bejaia.dz>
2023-05-13 00:13:22 +0100

    deco google

--- src/service/Caller.service.js
+++ src/service/Caller.service.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { accountService } from "./Account.service";
+import { Navigate } from "react-router-dom";
 
 const Axios = axios.create({ proxy:"http://localhost:9630"})
 Axios.interceptors.request.use(request => {
@@ -9,11 +10,12 @@ Axios.interceptors.request.use(request => {
 })
 Axios.interceptors.response.use(response => {
     return response
-}, error => {
-    if (error.response.status === 403) {
-        accountService.logout()
-        window.location = '/auth'
-    } else return Promise.reject(error)
-}
+    }, error => {
+        if (error.response.status === 403) {
+            accountService.logout()
+            Navigate('/auth')
+            // window.location = '/auth'
+        } else return Promise.reject(error)
+    }
 )
 export default Axios;
