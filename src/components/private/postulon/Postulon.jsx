9a217b9f5bc50ec79b93123125a3e40755e86933
Yaici-Yacine <yacine.yaici@se.univ-bejaia.dz>
2023-05-13 23:54:29 +0100

    test

--- src/components/private/postulon/Postulon.jsx
+++ src/components/private/postulon/Postulon.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 
-export default function Postulon() {
+export default function Postulon({Postulon}) {
+    const { adresseMail,nom,prenom,genre,tel,adresse,cv }=Postulon
     return (
         <div>
-            <h3>adresseMail : <span>amine@gmail.com</span></h3>
-            <h3>nom : <span>yacine</span></h3>
-            <h3>prenom : <span>zenati</span></h3>
-            <h3>genre : <span>HOMME</span></h3>
-            <h3>tel : <span></span></h3>
-            <h3>adresse : <span></span></h3>
-            <h3>cv : <a>cv.png</a></h3>
+            <h3>adresseMail : <span>{adresseMail}</span></h3>
+            <h3>nom : <span>{nom}</span></h3>
+            <h3>prenom : <span>{prenom}</span></h3>
+            <h3>genre : <span>{genre}</span></h3>
+            <h3>tel : <span>{tel}</span></h3>
+            {/* <h3>adresse : <span>{adresse.wilaya+" "+adresse.commune}</span></h3> */}
+            <h3>cv : <a target='_blank'>{cv}</a></h3>
         </div>
     )
 }
