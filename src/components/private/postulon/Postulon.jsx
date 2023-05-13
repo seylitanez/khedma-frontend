192f6bb32cf37acbe406fb875fa0130f0db0ad0a
Yaici-Yacine <yacine.yaici@se.univ-bejaia.dz>
2023-05-13 23:41:04 +0100

    test

---
+++ src/components/private/postulon/Postulon.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Postulon() {
+    return (
+        <div>
+            <h3>adresseMail : <span>amine@gmail.com</span></h3>
+            <h3>nom : <span>yacine</span></h3>
+            <h3>prenom : <span>zenati</span></h3>
+            <h3>genre : <span>HOMME</span></h3>
+            <h3>tel : <span></span></h3>
+            <h3>adresse : <span></span></h3>
+            <h3>cv : <a>cv.png</a></h3>
+        </div>
+    )
+}
