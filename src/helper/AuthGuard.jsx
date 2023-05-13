6ba7b51b4865cf2e1582a43d22c9eef8ccb1ca07
Yaici-Yacine <yacine.yaici@se.univ-bejaia.dz>
2023-05-13 22:02:33 +0100

    test

--- src/helper/AuthGuard.jsx
+++ src/helper/AuthGuard.jsx
@@ -3,5 +3,5 @@ import React from 'react'
 import { Navigate } from 'react-router-dom';
 
 export default function AuthGuard({role,children}) {
-    return (accountService.isLogged()&&role===accountService.getRole())?children:<Navigate to="/auth"/>
+    return (accountService.isValid()&&accountService.isLogged()&&role===accountService.getRole())?children:<Navigate to="/auth"/>
 }
--- src/service/Account.service.js
+++ src/service/Account.service.js
@@ -48,8 +48,7 @@ let getUserName = () => {
     return jwt_decode(getToken()).username;
 }
 let isValid=()=>{
-
-    if(getToken()) return jwt_decode(getToken()).valid;
+    return isLogged? jwt_decode(getToken()).valid:false;
 }
 let genrateToken=(information)=>{
     const ALGO="HS256"
